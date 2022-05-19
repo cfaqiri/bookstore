c2c68548fde56f10220e143ec98e1f1162854b18
Crystal Faqiri <cfaqiri@gmail.com>
2022-05-18 21:54:36 -0400

    Add test for deleting a book

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -14,12 +14,24 @@ describe 'Books API', type: :request do
   end
 
   describe 'POST /books' do
-    it 'create a new book' do
+    it 'creates a new book' do
       expect {
         post '/api/v1/books', params: {book: {title: 'The Martian', author: 'Andy Weir'}}
       }.to change { Book.count }.from(0).to(1)
     
       expect(response).to have_http_status(:created)
     end 
+  end
+
+  describe 'DELETE /books/:id' do
+    let!(:book) { FactoryBot.create(:book, title: "Time Machine", author: "George") }
+    
+    it 'deletes a book' do
+      expect {
+        delete "/api/v1/books/#{book.id}"
+      }.to change { Book.count }.from(1).to(0)
+
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
