cf58a2f32f9355b96f16ce1ed90c6c11b4177036
Crystal Faqiri <cfaqiri@gmail.com>
2022-05-18 21:46:21 -0400

    Add test for creating a new book

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  it 'returns all the books' do
-    FactoryBot.create(:book, title: "Time Machine", author: "George")
-    FactoryBot.create(:book, title: "Yo yo", author: "Gorge")
+  describe 'GET /books' do
+    it 'returns all the books' do
+      FactoryBot.create(:book, title: "Time Machine", author: "George")
+      FactoryBot.create(:book, title: "Yo yo", author: "Gorge")
 
-    get '/api/v1/books'
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+      expect {
+        post '/api/v1/books', params: {book: {title: 'The Martian', author: 'Andy Weir'}}
+      }.to change { Book.count }.from(0).to(1)
+    
+      expect(response).to have_http_status(:created)
+    end 
   end
 end
