4042698de3295cb7e0d24fc676f1ce8c6278493d
Crystal Faqiri <cfaqiri@gmail.com>
2022-05-20 22:07:02 -0400

    Refactor book tests to acknowledge model associations

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,33 +1,62 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
+  let(:first_author) { FactoryBot.create(:author, first_name: "George", last_name: "Orwell", age: 46) }
+  let(:second_author) { FactoryBot.create(:author, first_name: "Harry", last_name: "Wells", age: 98) }
+  
   describe 'GET /books' do
     before do
-      FactoryBot.create(:book, title: "Time Machine", author: "George")
-      FactoryBot.create(:book, title: "Yo yo", author: "Gorge")
+      FactoryBot.create(:book, title: "Time Machine", author: first_author)
+      FactoryBot.create(:book, title: "Yo yo", author: second_author)
     end
     
     it 'returns all the books' do
-
       get '/api/v1/books'
 
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(2)
+      expect(JSON.parse(response.body)).to eq(
+        [
+          {
+            'id' => 1,
+            'title' => 'Time Machine',
+            'author_name' => 'George Orwell',
+            'author_age' => 46
+          },
+          {
+            'id' => 2,
+            'title' => 'Yo yo',
+            'author_name' => 'Harry Wells',
+            'author_age' => 98
+          }
+        ]
+      )
     end
   end
 
   describe 'POST /books' do
     it 'creates a new book' do
       expect {
-        post '/api/v1/books', params: {book: {title: 'The Martian', author: 'Andy Weir'}}
+        post '/api/v1/books', params: {
+          book: {title: 'The Martian'}, 
+          author: {first_name: 'Andy', last_name: 'Weir', age: '67'}}
       }.to change { Book.count }.from(0).to(1)
     
       expect(response).to have_http_status(:created)
+      expect(Author.count).to eq(1)
+      expect(JSON.parse(response.body)).to eq(
+        {
+          'id' => 1,
+          'title' => 'The Martian',
+          'author_name' => 'Andy Weir',
+          'author_age' => 67
+        }
+      )
     end 
   end
 
   describe 'DELETE /books/:id' do
-    let!(:book) { FactoryBot.create(:book, title: "Time Machine", author: "George") }
+    let!(:book) { FactoryBot.create(:book, title: "Time Machine", author: first_author) }
     
     it 'deletes a book' do
       expect {
