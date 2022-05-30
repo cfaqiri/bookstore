0e8947e0cde03be5c40612ab508d3b3ae676ea62
Crystal Faqiri <cfaqiri@gmail.com>
2022-05-30 11:30:02 -0400

    Add password to user model

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Authentication", type: :request do
   describe "POST /authenticate" do
-    let(:user) { FactoryBot.create(:user, username: 'BookSeller99') }
+    let(:user) { FactoryBot.create(:user, username: 'BookSeller99', password: 'Password1') }
 
     it "authenticates the client" do
       post "/api/v1/authenticate",
@@ -12,7 +12,7 @@ describe "Authentication", type: :request do
            }
 
       expect(response).to have_http_status(:created)
-      expect(response_body).to eq({ "token" => "123" })
+      expect(response_body).to eq({ "token" => "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w" })
     end
 
     it "returns error when username is missing" do
@@ -31,6 +31,12 @@ describe "Authentication", type: :request do
       expect(response_body).to eq(
         { "error" => "param is missing or the value is empty: password" }
       )
+    end
+
+    it "returns error when password is incorrect" do
+      post "/api/v1/authenticate", params: { username: user.username, password: "incorrect"}
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
